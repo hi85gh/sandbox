5591a78f101cc53bc815ed04f10bef75dbc575ad
hi85 <33993836+hi85gh@users.noreply.github.com>
2020-09-28 23:41:36 +0900

    chore(vue.js/components): add AppHeading component

--- src/vue.js/components/src/App.vue
+++ src/vue.js/components/src/App.vue
@@ -1,11 +1,15 @@
 <script lang="ts">
 import { defineComponent } from 'vue'
+import AppHeading from './components/AppHeading.vue'
 
 export default defineComponent({
   name: 'App',
+  components: {
+    AppHeading,
+  },
 })
 </script>
 
 <template>
-  <div />
+  <AppHeading tag="h1">AppHeading component</AppHeading>
 </template>
