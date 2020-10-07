43ce110b8e1491d2b83e1d334e78ff5f9482e20c
hi85 <33993836+hi85gh@users.noreply.github.com>
2020-10-07 23:51:43 +0900

    chore(vue.js/components): update AppInput component

--- src/vue.js/components/src/components/AppInput.ts
+++ src/vue.js/components/src/components/AppInput.ts
@@ -1,28 +1,69 @@
-import { defineComponent, h } from 'vue'
+import { Prop, PropType, defineComponent, h } from 'vue'
+
+const allowTypes = [
+  'date',
+  'datetime',
+  'email',
+  'month',
+  'number',
+  'password',
+  'search',
+  'tel',
+  'text',
+  'time',
+  'url',
+  'week',
+] as const
+
+/** 対応する v-model 修飾子 */
+type ModelModifier = 'number'
+
+/** type 属性で "button", "checkbox", "color", "file", "hidden", "image", "radio", "range", "reset", "submit" は許容しない */
+type AllowType = typeof allowTypes[number]
+
+const defaultType: AllowType = 'text'
 
 export default defineComponent({
   name: 'AppInput',
   inheritAttrs: false,
   props: {
-    modelValue: {
-      required: false,
+    modelValue: {} as Prop<any>,
+    modelModifiers: {
+      type: Object as PropType<{ [key in ModelModifier]?: boolean }>,
+      default: () => ({}),
+    },
+    type: {
+      type: String as PropType<AllowType>,
+      default: defaultType,
+      validator: (value: AllowType) => allowTypes.includes(value),
     },
   },
   emits: {
-    'update:modelValue': null,
+    'update:modelValue': (payload: string | number) =>
+      /* v-model に number 修飾子が指定された場合は引数は数値とする */
+      typeof payload === 'string' || typeof payload === 'number',
   },
   setup(props, { attrs, emit }) {
-    function onInput(event: Event) {
-      if (event.target instanceof HTMLInputElement) {
-        emit('update:modelValue', event.target.value)
-      }
-    }
-
     return () =>
       h('input', {
         ...attrs,
+        type: allowTypes.includes(props.type) ? props.type : defaultType,
         value: props.modelValue,
-        onInput,
+        onInput: (event: Event) => {
+          if (event.target instanceof HTMLInputElement) {
+            if (props.modelModifiers.number) {
+              /* v-model に number 修飾子が指定された場合の処理 */
+              const parsedValue = parseFloat(event.target.value)
+
+              if (Number.isFinite(parsedValue)) {
+                emit('update:modelValue', parsedValue)
+                return
+              }
+            }
+
+            emit('update:modelValue', event.target.value)
+          }
+        },
       })
   },
 })
