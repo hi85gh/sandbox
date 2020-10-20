98c3d68e2fd6c313e7a1d5ab458242c008751b39
hi85 <33993836+hi85gh@users.noreply.github.com>
2020-10-20 17:44:43 +0900

    chore(typescript/ts-jest): enable completion of jest.config.js

--- src/typescript/packages/ts-jest/jest.config.js
+++ src/typescript/packages/ts-jest/jest.config.js
@@ -1,4 +1,12 @@
-module.exports = {
+/**
+ * Reference: https://kulshekhar.github.io/ts-jest/user/config/#ide-ts-jest-config-suggestion
+ *
+ * @typedef {import('ts-jest')}
+ * @type {import('@jest/types').Config.InitialOptions}
+ */
+const config = {
   preset: 'ts-jest',
   testEnvironment: 'node',
-};
+}
+
+module.exports = config
