6795e38d68cf7593d26b1708cb81fe57a83f9c23
hi85 <33993836+hi85gh@users.noreply.github.com>
2020-10-20 19:03:25 +0900

    chore(typescript/ts-jest): add scripts

---
+++ src/typescript/packages/ts-jest/src/is-number.spec.ts
@@ -0,0 +1,33 @@
+import { isNumber } from '~/is-number'
+
+test('Expect true.', () => {
+  expect(isNumber(0)).toBe(true)
+
+  expect(isNumber(1)).toBe(true)
+
+  expect(isNumber(-1)).toBe(true)
+
+  expect(isNumber(1.01)).toBe(true)
+
+  expect(isNumber(-1.01)).toBe(true)
+})
+
+test('Expect false.', () => {
+  expect(isNumber(null)).toBe(false)
+
+  expect(isNumber(undefined)).toBe(false)
+
+  expect(isNumber(NaN)).toBe(false)
+
+  expect(isNumber(true)).toBe(false)
+
+  expect(isNumber(false)).toBe(false)
+
+  expect(isNumber('string')).toBe(false)
+
+  expect(isNumber([])).toBe(false)
+
+  expect(isNumber({})).toBe(false)
+
+  expect(isNumber(() => {})).toBe(false)
+})
