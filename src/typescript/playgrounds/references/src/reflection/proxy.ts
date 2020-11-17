83029edbfc9b07c0cea42d01f307ffb38a233552
hi85 <33993836+hi85gh@users.noreply.github.com>
2020-11-17 23:45:18 +0900

    chore(typescript/references): add scripts

---
+++ src/typescript/playgrounds/references/src/reflection/proxy.ts
@@ -0,0 +1,49 @@
+const target = {
+  message: 'Hello, World!',
+}
+
+/**
+ * `ProxyHandler` の利用条件
+ * - `tsconfig.json` で `compilerOptions.target` が `ES2015` (ES6) 以上であること
+ */
+const handler: ProxyHandler<typeof target> = {
+  set: (obj, prop, value) => {
+    /**
+     * 値の検証など処理を入れることが可能。
+     */
+    if (typeof value !== 'string') {
+      return false
+    }
+
+    /**
+     * `compilerOptions.target.noImplicitAny` (`compilerOptions.target.strict`) が `true` の場合、以下の記述はエラーとなる。
+     *
+     * ```ts
+     * // Error
+     * obj[prop] = value
+     * ```
+     *
+     * 以下のようにアサーションを使用して回避することもできるが `Reflect` を使用する。
+     *
+     * ```ts
+     * // Bad
+     * obj[prop as keyof typeof obj] = value
+     * ```
+     */
+    return Reflect.set(obj, prop, value)
+  },
+}
+
+const proxy = new Proxy(target, handler)
+
+try {
+  // @ts-ignore
+  proxy.message = 0
+} catch (error) {
+  console.error(error.message)
+} finally {
+  console.log(proxy.message) // 値は 'Hello, World!' のまま
+}
+
+proxy.message = 'Hello, New World!'
+console.log(proxy.message) // 値は 'Hello, New World!' に上書きされている
