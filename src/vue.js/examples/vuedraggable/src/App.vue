bfd2d6ab44437a659472e78eeee8df3c324c3b6e
hi85 <33993836+hi85gh@users.noreply.github.com>
2020-12-07 06:30:58 +0900

    chore(vue.js/vuedraggable): add scripts

--- src/vue.js/examples/vuedraggable/src/App.vue
+++ src/vue.js/examples/vuedraggable/src/App.vue
@@ -1,9 +1,11 @@
 <script lang="ts">
 import { defineComponent } from 'vue'
+import Lists from './Lists.vue'
 import Simple from './Simple.vue'
 
 export default defineComponent({
   components: {
+    Lists,
     Simple,
   },
 })
@@ -16,4 +18,11 @@ export default defineComponent({
     </a>
   </p>
   <Simple />
+  <hr />
+  <p>
+    <a href="https://sortablejs.github.io/vue.draggable.next/#/two-lists">
+      Lists
+    </a>
+  </p>
+  <Lists />
 </template>
---
+++ src/vue.js/examples/vuedraggable/src/Lists.vue
@@ -0,0 +1,80 @@
+<script lang="ts">
+import { GroupOptions } from 'sortablejs'
+import { computed, defineComponent, reactive, toRefs } from 'vue'
+import draggable from 'vuedraggable'
+
+export default defineComponent({
+  components: {
+    draggable,
+  },
+  setup() {
+    const state = reactive({
+      foo: [...Array(5)].map((_, index) => ({ id: ++index, list: 'foo' })),
+      bar: [...Array(5)].map((_, index) => ({ id: ++index, list: 'bar' })),
+      baz: [...Array(5)].map((_, index) => ({ id: ++index, list: 'baz' })),
+    })
+
+    return {
+      ...toRefs(state),
+      group: computed<GroupOptions>(() => ({ name: 'lists' })),
+    }
+  },
+})
+</script>
+
+<template>
+  <div class="container">
+    <draggable v-model="foo" group="group" item-key="id">
+      <template #item="{ element }">
+        <div class="item">{{ element.list }}{{ element.id }}</div>
+      </template>
+    </draggable>
+    <draggable v-model="bar" group="group" item-key="id">
+      <template #item="{ element }">
+        <div class="item">{{ element.list }}{{ element.id }}</div>
+      </template>
+    </draggable>
+    <draggable v-model="baz" group="group" item-key="id">
+      <template #item="{ element }">
+        <div class="item">{{ element.list }}{{ element.id }}</div>
+      </template>
+    </draggable>
+  </div>
+</template>
+
+<style scoped>
+.container {
+  display: flex;
+}
+
+.container > * {
+  box-sizing: border-box;
+  min-width: 140px;
+  padding: 10px;
+}
+
+.container > :nth-child(1) {
+  background-color: #f00;
+}
+
+.container > :nth-child(2) {
+  background-color: #0f0;
+}
+
+.container > :nth-child(3) {
+  background-color: #00f;
+}
+
+.item {
+  box-sizing: border-box;
+  display: inline-flex;
+  align-items: center;
+  justify-content: center;
+  width: 100px;
+  height: 100px;
+  margin: 10px;
+  font-size: 30px;
+  color: #fff;
+  background-color: #000;
+}
+</style>
