d180626bd82eb41c0b62878814e31bf535d37c9b
hi85 <33993836+hi85gh@users.noreply.github.com>
2020-12-12 14:15:53 +0900

    chore(nuxt.js/vuetify): reduce code

--- src/nuxt.js/examples/vuetify/nuxt.config.js
+++ src/nuxt.js/examples/vuetify/nuxt.config.js
@@ -1,47 +1,9 @@
 import colors from 'vuetify/es5/util/colors'
 
 export default {
-  // Target (https://go.nuxtjs.dev/config-target)
-  target: 'static',
-
-  // Global page headers (https://go.nuxtjs.dev/config-head)
-  head: {
-    titleTemplate: '%s - vuetify',
-    title: 'vuetify',
-    meta: [
-      { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: '' }
-    ],
-    link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
-    ]
-  },
-
-  // Global CSS (https://go.nuxtjs.dev/config-css)
-  css: [
-  ],
-
-  // Plugins to run before rendering page (https://go.nuxtjs.dev/config-plugins)
-  plugins: [
-  ],
-
-  // Auto import components (https://go.nuxtjs.dev/config-components)
+  buildModules: ['@nuxt/typescript-build', '@nuxtjs/vuetify'],
   components: true,
-
-  // Modules for dev and build (recommended) (https://go.nuxtjs.dev/config-modules)
-  buildModules: [
-    // https://go.nuxtjs.dev/typescript
-    '@nuxt/typescript-build',
-    // https://go.nuxtjs.dev/vuetify
-    '@nuxtjs/vuetify',
-  ],
-
-  // Modules (https://go.nuxtjs.dev/config-modules)
-  modules: [
-  ],
-
-  // Vuetify module configuration (https://go.nuxtjs.dev/config-vuetify)
+  target: 'static',
   vuetify: {
     customVariables: ['~/assets/variables.scss'],
     theme: {
@@ -54,13 +16,9 @@ export default {
           info: colors.teal.lighten1,
           warning: colors.amber.base,
           error: colors.deepOrange.accent4,
-          success: colors.green.accent3
-        }
-      }
-    }
+          success: colors.green.accent3,
+        },
+      },
+    },
   },
-
-  // Build Configuration (https://go.nuxtjs.dev/config-build)
-  build: {
-  }
 }
