6d1f5e898eefc6361fa127ac57c190ba5fd2070a
hi85 <33993836+hi85gh@users.noreply.github.com>
2020-12-12 14:33:24 +0900

    chore(nuxt.js/vuetify): add vuetify.options.ts

--- src/nuxt.js/examples/vuetify/nuxt.config.ts
+++ src/nuxt.js/examples/vuetify/nuxt.config.ts
@@ -1,5 +1,4 @@
 import type { NuxtConfig } from '@nuxt/types'
-import colors from 'vuetify/es5/util/colors'
 
 const config: NuxtConfig = {
   buildModules: ['@nuxt/typescript-build', '@nuxtjs/vuetify'],
@@ -7,21 +6,7 @@ const config: NuxtConfig = {
   target: 'static',
   vuetify: {
     customVariables: ['~/assets/variables.scss'],
-    theme: {
-      dark: true,
-      // @ts-expect-error
-      themes: {
-        dark: {
-          primary: colors.blue.darken2,
-          accent: colors.grey.darken3,
-          secondary: colors.amber.darken3,
-          info: colors.teal.lighten1,
-          warning: colors.amber.base,
-          error: colors.deepOrange.accent4,
-          success: colors.green.accent3,
-        },
-      },
-    },
+    optionsPath: '~~/vuetify.options.ts',
   },
 }
 
---
+++ src/nuxt.js/examples/vuetify/vuetify.options.ts
@@ -0,0 +1,21 @@
+import colors from 'vuetify/es5/util/colors'
+import type { GlobalVuetifyPreset } from 'vuetify/types/services/presets'
+
+const options: GlobalVuetifyPreset = {
+  theme: {
+    dark: true,
+    themes: {
+      dark: {
+        primary: colors.blue.darken2,
+        accent: colors.grey.darken3,
+        secondary: colors.amber.darken3,
+        info: colors.teal.lighten1,
+        warning: colors.amber.base,
+        error: colors.deepOrange.accent4,
+        success: colors.green.accent3,
+      },
+    },
+  },
+}
+
+export default options
