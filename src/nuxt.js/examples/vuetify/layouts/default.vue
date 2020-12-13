922da8256e8ed497c2791d7bb3d202c8290515de
hi85 <33993836+hi85gh@users.noreply.github.com>
2020-12-13 23:39:24 +0900

    chore(nuxt.js/vuetify): update scripts

--- src/nuxt.js/examples/vuetify/layouts/default.vue
+++ src/nuxt.js/examples/vuetify/layouts/default.vue
@@ -1,3 +1,65 @@
+<script lang="ts">
+import {
+  defineComponent,
+  getCurrentInstance,
+  onMounted,
+  useMeta,
+} from '@nuxtjs/composition-api'
+import type { MetaInfo } from 'vue-meta'
+
+const noFoucClassName = 'no-fouc'
+
+/**
+ * `universal` モードで `generate` した場合に FOUC が発生するのを防ぐ
+ *
+ * Reference: https://github.com/nuxt-community/vuetify-module/issues/232
+ */
+const head: MetaInfo = {
+  style: [
+    {
+      cssText: `.${noFoucClassName} { display: none }`,
+      type: 'text/css',
+    },
+  ],
+  script: [
+    {
+      hid: 'noFoucHead',
+      pbody: true, // <body> の直後に追加
+      innerHTML: `document.body.className='${noFoucClassName} '+document.body.className`, // エスケープの無効化
+    },
+  ],
+
+  /**
+   * `<script>` タグの埋め込み
+   *
+   * Reference: https://vue-meta.nuxtjs.org/api/#dangerouslydisablesanitizersbytagid
+   */
+  __dangerouslyDisableSanitizersByTagID: {
+    noFoucHead: ['innerHTML'],
+  },
+}
+
+export default defineComponent({
+  setup(_, { root }) {
+    if (process.server) {
+      /* <script> タグが 2 回追加されないよう Server side でのみ実行 */
+      useMeta(head)
+    }
+
+    onMounted(async () => {
+      await root.$nextTick()
+
+      /* 追加した class を Client side で削除 */
+      document.body.className = document.body.className.replace(
+        `${noFoucClassName} `,
+        ''
+      )
+    })
+  },
+  head: {}, // Required for useMeta
+})
+</script>
+
 <template>
   <VApp>
     <VAppBar app>
@@ -11,9 +73,3 @@
     </VFooter>
   </VApp>
 </template>
-
-<script lang="ts">
-import Vue from 'vue'
-
-export default Vue.extend({})
-</script>
