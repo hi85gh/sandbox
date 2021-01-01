4ce4dade0cbcde76175d54a1f7f22907b919e941
hi85 <33993836+hi85gh@users.noreply.github.com>
2021-01-01 21:26:41 +0900

    chore(vue.js/vite): update vite.config.ts

--- src/vue.js/examples/vite/vite.config.ts
+++ src/vue.js/examples/vite/vite.config.ts
@@ -3,16 +3,24 @@ import { UserConfig } from 'vite/dist/node/config'
 
 const config: UserConfig = {
   alias: {
-    '/@/': resolve(__dirname, './src'),
+    '/@/': resolve(__dirname, 'src'),
   },
   define: {
     /**
      * Disable Options API
-     * Referred: https://github.com/vuejs/vue-next/tree/master/packages/vue#bundler-build-feature-flags
+     * Reference: https://github.com/vuejs/vue-next/tree/master/packages/vue#bundler-build-feature-flags
      */
     __VUE_OPTIONS_API__: false,
   },
   outDir: resolve(__dirname, 'dist'),
+  resolvers: [
+    {
+      /**
+       * Reference: https://github.com/vitejs/vite/issues/300#issuecomment-708944488
+       */
+      alias: (id) => id.replace(/^@\//, '/@/'),
+    },
+  ],
   root: resolve(__dirname, 'src'),
 }
 
