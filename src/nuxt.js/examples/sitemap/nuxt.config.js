958091ae438d127fa3a37022f3efc27bdd64f57b
hi85 <33993836+hi85gh@users.noreply.github.com>
2021-01-06 23:46:15 +0900

    chore(nuxt.js/sitemap): set up @nuxtjs/sitemap

--- src/nuxt.js/examples/sitemap/nuxt.config.js
+++ src/nuxt.js/examples/sitemap/nuxt.config.js
@@ -1,5 +1,9 @@
 export default {
   buildModules: ['@nuxt/typescript-build'],
   components: true,
+  modules: ['@nuxtjs/sitemap'],
+  sitemap: {
+    hostname: 'http://localhost/', // required
+  },
   target: 'static',
 }
