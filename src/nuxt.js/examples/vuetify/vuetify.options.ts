127d0a6959bb7c0cede8cd5666300385e05ed07c
hi85 <33993836+hi85gh@users.noreply.github.com>
2021-01-11 21:50:59 +0900

    chore(nuxt.js/vuetify): add lang option

--- src/nuxt.js/examples/vuetify/vuetify.options.ts
+++ src/nuxt.js/examples/vuetify/vuetify.options.ts
@@ -1,8 +1,19 @@
+/**
+ * References:
+ * - https://vuetifyjs.com/en/features/internationalization/
+ * - https://github.com/nuxt-community/vuetify-module/issues/198
+ */
+// @ts-expect-error
+import { en, ja } from 'vuetify/lib/locale' // Not 'vuetify/src/locale'
 import type { GlobalVuetifyPreset } from 'vuetify/types/services/presets'
 
 const options: GlobalVuetifyPreset = {
   icons: {
     iconfont: 'mdiSvg',
+  },
+  lang: {
+    locales: { en, ja },
+    current: 'ja',
   },
   theme: {
     dark: true,
