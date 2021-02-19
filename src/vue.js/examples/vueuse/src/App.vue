6b70df043866f2c32a2bad4ffd7810ed5dc6a470
hi85 <33993836+hi85gh@users.noreply.github.com>
2021-02-19 19:39:33 +0900

    chore(vue.js/vueuse): try useDebounce function

--- src/vue.js/examples/vueuse/src/App.vue
+++ src/vue.js/examples/vueuse/src/App.vue
@@ -1,10 +1,12 @@
 <script lang="ts">
 import { defineComponent } from 'vue'
+import Debounce from './components/Debounce.vue'
 import LocalStorage from './components/LocalStorage.vue'
 import Mouse from './components/Mouse.vue'
 
 export default defineComponent({
   components: {
+    Debounce,
     LocalStorage,
     Mouse,
   },
@@ -13,6 +15,10 @@ export default defineComponent({
 
 <template>
   <h1>Hello Vue 3 + TypeScript + Vite</h1>
+  <hr />
+  <a href="https://vueuse.org/shared/useDebounce/">useDebounce</a>
+  <Debounce />
+
   <hr />
   <a href="https://vueuse.org/core/useLocalStorage/">useLocalStorage</a>
   <LocalStorage />
---
+++ src/vue.js/examples/vueuse/src/components/Debounce.vue
@@ -0,0 +1,20 @@
+<script lang="ts">
+import { defineComponent, ref } from 'vue'
+import { useDebounce } from '@vueuse/core'
+
+export default defineComponent({
+  setup() {
+    const input = ref('')
+    const debouncedInput = useDebounce(input, 500)
+
+    return { debouncedInput, input }
+  },
+})
+</script>
+
+<template>
+  <p>Debounced input: {{ debouncedInput || '""' }}</p>
+  <p>
+    <input v-model="input" type="text" />
+  </p>
+</template>
