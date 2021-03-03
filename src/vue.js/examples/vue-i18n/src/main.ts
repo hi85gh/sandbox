bfc6e78bb2feafe41fd561267bb1f498a885d3eb
hi85 <33993836+hi85gh@users.noreply.github.com>
2021-03-03 09:57:22 +0900

    chore(vite/vue-i18n): set up vue-i18n

--- src/vue.js/examples/vue-i18n/src/main.ts
+++ src/vue.js/examples/vue-i18n/src/main.ts
@@ -1,4 +1,16 @@
 import { createApp } from 'vue'
+import { createI18n } from 'vue-i18n'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+const i18n = createI18n({
+  locale: 'en',
+  fallbackLocale: 'en',
+
+  /**
+   * Must be set to `false` to use Composition API
+   * @see {@link https://vue-i18n-next.intlify.dev/guide/advanced/composition.html#basic-usage Vue I18n}
+   */
+  legacy: false,
+})
+
+createApp(App).use(i18n).mount('#app')
