0cf68abef92f78ef238811d621e096e8b8382586
hi85 <33993836+hi85gh@users.noreply.github.com>
2021-03-04 00:52:31 +0900

    chore(vite/vue-i18n): try custom blocks

--- src/vue.js/examples/vue-i18n/src/App.vue
+++ src/vue.js/examples/vue-i18n/src/App.vue
@@ -1,14 +1,21 @@
 <template>
+  <h2>Simple usage</h2>
   <Simple />
+  <hr />
+
+  <h2>Use custom blocks</h2>
+  <CustomBlock />
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue'
+import CustomBlock from './components/CustomBlock.vue'
 import Simple from './components/Simple.vue'
 
 export default defineComponent({
   name: 'App',
   components: {
+    CustomBlock,
     Simple,
   },
 })
---
+++ src/vue.js/examples/vue-i18n/src/components/CustomBlock.vue
@@ -0,0 +1,32 @@
+<script lang="ts">
+import { defineComponent } from 'vue'
+import { useI18n } from 'vue-i18n'
+
+export default defineComponent({
+  setup() {
+    const { locale, t } = useI18n()
+
+    return { locale, t }
+  },
+})
+</script>
+
+<template>
+  <p>message: {{ t('message') }}</p>
+  <select v-model="locale">
+    <option v-for="language in ['en', 'ja']" :key="language">
+      {{ language }}
+    </option>
+  </select>
+</template>
+
+<i18n>
+{
+  "en": {
+    "message": "Hello, World!"
+  },
+  "ja": {
+    "message": "こんにちは世界！"
+  }
+}
+</i18n>
