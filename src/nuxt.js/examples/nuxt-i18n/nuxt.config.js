c0670b87680ab2f645faaf8ad1bb552bad199524
hi85 <33993836+hi85gh@users.noreply.github.com>
2021-03-05 06:42:36 +0900

    chore(nuxt.js/nuxt-i18n): set up nuxt-i18n

--- src/nuxt.js/examples/nuxt-i18n/nuxt.config.js
+++ src/nuxt.js/examples/nuxt-i18n/nuxt.config.js
@@ -1,5 +1,17 @@
 export default {
   buildModules: ['@nuxt/typescript-build'],
   components: true,
+  i18n: {
+    defaultLocale: 'en',
+    locales: ['en', 'ja'],
+    vueI18n: {
+      fallbackLocale: 'en',
+      messages: {
+        en: { greeting: 'Hello, World!' },
+        ja: { greeting: 'こんにちは世界！' },
+      },
+    },
+  },
+  modules: ['nuxt-i18n'],
   target: 'static',
 }
