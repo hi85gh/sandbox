e97f61310910b98ecd59c3861195c7cd4fd49388
hi85 <33993836+hi85gh@users.noreply.github.com>
2021-03-05 09:09:09 +0900

    chore(nuxt.js/nuxt-i18n): add an example of locale change

--- src/nuxt.js/examples/nuxt-i18n/pages/index.vue
+++ src/nuxt.js/examples/nuxt-i18n/pages/index.vue
@@ -3,6 +3,16 @@
     <div>
       <h1>nuxt-i18n</h1>
       <p>{{ $t('greeting') }}</p>
+      <div>
+        <p>Locale: {{ $i18n.locale }}</p>
+        <p>
+          <select v-model="$i18n.locale">
+            <option v-for="locale in locales" :key="locale">
+              {{ locale }}
+            </option>
+          </select>
+        </p>
+      </div>
     </div>
   </div>
 </template>
@@ -10,5 +20,13 @@
 <script lang="ts">
 import Vue from 'vue'
 
-export default Vue.extend({})
+export default Vue.extend({
+  computed: {
+    locales(): string[] {
+      return (this.$i18n.locales || []).map(locale =>
+        typeof locale === 'string' ? locale : locale.code
+      )
+    }
+  }
+})
 </script>
